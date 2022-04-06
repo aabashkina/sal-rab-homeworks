c381607465f44852f6fae1095e18e51fdac858b6
marsen <aabashkina@mail.ru>
2022-04-06 15:14:52 +0300

    task1

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,20 +3,10 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-    message = productName;
-    let message = productName + "за" + productPrice + "теперь в корзине!";
+    let message = productName;
+    message = productName + " " + "за" + " " + productPrice + " " + "теперь в корзине!";
     console.log(message);
-    
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
-
+       
     // Конец решения задания №1.1.
 
     return message;
@@ -27,7 +17,7 @@ function addInCartCountChange(value) {
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
     let newValue = oldValue;
-    value = newValue + 1;
+    newValue = newValue + 1;
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
@@ -42,11 +32,10 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
-    let newSum;
-    newSum = oldSum + difference;;
-    let text = "₽"
+    let newSum = oldSum + difference;
+    let text = " ₽"
     let newSumText = `newSum${text}`;
-    let string1 = newSum + text;
+    newSumText = newSum + text;
     
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
