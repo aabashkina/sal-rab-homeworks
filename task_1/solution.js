49f2c8e62e8125a0a23dc734be5cb2aab7cf6db8
marsen <aabashkina@mail.ru>
2022-04-06 15:16:52 +0300

    aabashkina

--- task_1/solution.js
+++ task_1/solution.js
@@ -18,10 +18,6 @@ function addInCartCountChange(value) {
     let oldValue = value;
     let newValue = oldValue;
     newValue = newValue + 1;
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
-
     // Конец решения задания №1.2.
 
     return newValue;
@@ -36,11 +32,6 @@ function addInCartSumChange(sum, delta) {
     let text = " ₽"
     let newSumText = `newSum${text}`;
     newSumText = newSum + text;
-    
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
-
     // Конец решения задания №1.3.
 
     return newSumText;
