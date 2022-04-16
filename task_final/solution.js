41a98cac03156b5c6c833837a0d849a60da24d79
marsen <aabashkina@mail.ru>
2022-04-16 21:25:48 +0300

    final task

--- task_final/solution.js
+++ task_final/solution.js
@@ -24,30 +24,34 @@
 //        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
 //        "sum": 900
 //       },
-//       "goods": [
+//       "goods": 
 //         {
 //           "title": "Пицца",
 //           "count": 2
 //         }
-//      ]
+//      
 //    }
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: '', order: {address, sum}, goods: []};
+    
     let countOfGoods = goods.length;
+    
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push(
+            {
+            'title': goods[i].title,
+            'count': goods[i].count
+        });
+     }
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+    data.client = `${name} ${phone}`;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = sum;
+      
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
